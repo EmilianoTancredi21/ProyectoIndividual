a59d90a2905e5685782e66dd76d8c36dae3e9cac
EmilianoTancredi21 <emitancredi2015@gmail.com>
2023-08-31 16:07:29 -0300

    fix

--- client/src/components/views/Details/Details.jsx
+++ client/src/components/views/Details/Details.jsx
@@ -3,7 +3,9 @@ import styles from "../Details/Details.module.css";
 import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import App from "../../../App"
+import App from "../../../App";
+
+const URL = "https://pi-server-jyus.onrender.com";
 
 const noImage =
   "https://i.pinimg.com/736x/a4/fc/6c/a4fc6c0d3d05fe453cdbafba248ae20c.jpg";
@@ -13,11 +15,10 @@ const Details = () => {
   const { id } = useParams();
   const [driver, setDriver] = useState({});
 
-
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get(`http://localhost:3001/drivers/${id}`);
+        const response = await axios.get(`${URL}/${id}`);
         const data = response.data;
         if (Object.keys(data).length === 0) {
           setDriver(null);
@@ -55,17 +56,25 @@ const Details = () => {
             </h2>
             <h2 className={styles.title}>{driver.dob}</h2>
             <h2 className={styles.teams}>
-              Teams: {" "}
-              {driver.teams ? driver.teams : driver.Teams?.map((teams) => teams.name).join(", ")}
+              Teams:{" "}
+              {driver.teams
+                ? driver.teams
+                : driver.Teams?.map((teams) => teams.name).join(", ")}
             </h2>
-                <h2 className={styles.titleID}>Driver ID: {driver.id}</h2>
-                <div>
-                  <button>
-                  <Link to="/home" element={<App />} style={{ textDecoration: 'none' }} className={styles.buttonDetail}>Go back</Link>
-                  </button>
-                </div>
+            <h2 className={styles.titleID}>Driver ID: {driver.id}</h2>
+            <div>
+              <button>
+                <Link
+                  to="/home"
+                  element={<App />}
+                  style={{ textDecoration: "none" }}
+                  className={styles.buttonDetail}
+                >
+                  Go back
+                </Link>
+              </button>
+            </div>
           </div>
-          
         </>
       ) : (
         <h2 className={styles.noDrivers}>No drivers found.</h2>
@@ -73,7 +82,5 @@ const Details = () => {
     </div>
   );
 };
-
-
 
 export default Details;
