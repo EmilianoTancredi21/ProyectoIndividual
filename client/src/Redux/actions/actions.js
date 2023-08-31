8cf5985756012ba3d89d7edfa8688dc54137f90a
EmilianoTancredi21 <emitancredi2015@gmail.com>
2023-08-31 15:54:35 -0300

    Add changes

--- client/src/Redux/actions/actions.js
+++ client/src/Redux/actions/actions.js
@@ -3,7 +3,7 @@ import axios from "axios";
 //Creamos las actions-types
 
 export const GET_ALL_DRIVERS = "GET_ALL_DRIVERS"; //
-export const GET_DRIVER_BYNAME = "GET_DRIVER_BYNAME";//
+export const GET_DRIVER_BYNAME = "GET_DRIVER_BYNAME"; //
 export const GET_TEAMS = "GET_TEAMS"; //
 export const FILTER_DRIVER_ALP = "FILTER_DRIVER_ALP";
 export const GET_FILTER_BY_TEAMS = "GET_FILTER_BY_TEAMS";
@@ -11,100 +11,92 @@ export const FILTER_BY_DOB = "FILTER_BY_DOB";
 export const GET_FILTER_CREATED_DRIVERS = "GET_FILTER_CREATED_DRIVERS";
 export const RESET_FILTER = "RESET_FILTER";
 
-
-
-
 //Creamos las actions
 
 export const getAllDrivers = () => {
-    return async (dispatch) => {
-        try {
-            const response = await axios(`http://localhost:3001/drivers`);
-            dispatch({
-                type: GET_ALL_DRIVERS,
-                payload: response.data
-            })
-        } catch (error) {
-            console.error(error);
-        }
+  return async (dispatch) => {
+    try {
+      const response = await axios(`http://localhost:3001/drivers`);
+      dispatch({
+        type: GET_ALL_DRIVERS,
+        payload: response.data,
+      });
+    } catch (error) {
+      console.error(error);
     }
-}
-
-
+  };
+};
 
 export const GetAllTeams = () => {
   return async (dispatch) => {
     try {
       const response = await axios.get(`http://localhost:3001/teams`);
-      dispatch({ 
+      dispatch({
         type: GET_TEAMS,
-        payload: response.data 
+        payload: response.data,
       });
     } catch (error) {
       console.error(error);
     }
-    };
   };
-  
-  
-  export const GetDriverByName = (name) => {
-    return async (dispatch) => {
-      try {
-        const response = await axios.get(`http://localhost:3001/drivers/name?name=${name}`);
-        dispatch({ 
-          type: GET_DRIVER_BYNAME, 
-          payload: response.data
-        });
-      } catch (error) {
-        console.error(error);
-        throw error;
-      }
-    };
-  };
-  
-  export const FilterByDob = (order) => {
-    return function (dispatch) {
-      try {
-        dispatch({ type: FILTER_BY_DOB, payload: order });
-      } catch (error) {
-        alert(error.response.data);
-      }
-    };
-  };
-  
-  
-  export const filterDriver = (order) => {
-    return (dispatch) => {
-      try {
-        dispatch({
-          type: FILTER_DRIVER_ALP,
-          payload: order
-        })
-      } catch (error) {
-        console.error(error);
-      }
+};
+
+export const GetDriverByName = (name) => {
+  return async (dispatch) => {
+    try {
+      const response = await axios.get(
+        `http://localhost:3001/drivers/name?name=${name}`
+      );
+      dispatch({
+        type: GET_DRIVER_BYNAME,
+        payload: response.data,
+      });
+    } catch (error) {
+      console.error(error);
+      throw error;
     }
-  }
-  
-  
-  export const GetFilterByTeam = (info) => ({  ///
-    type: GET_FILTER_BY_TEAMS,
-    payload: info
-  });
-
-
-
-  export function GetFilterCreatedDriver(infOrigin) { ///
-    return  {
-       type: GET_FILTER_CREATED_DRIVERS, 
-       payload: infOrigin 
-    };
-  }
-
-
-
-  export const resetFilters = () => {
-    return {
-      type: RESET_FILTER
-    };
   };
+};
+
+export const FilterByDob = (order) => {
+  return function (dispatch) {
+    try {
+      dispatch({ type: FILTER_BY_DOB, payload: order });
+    } catch (error) {
+      alert(error.response.data);
+    }
+  };
+};
+
+export const filterDriver = (order) => {
+  return (dispatch) => {
+    try {
+      dispatch({
+        type: FILTER_DRIVER_ALP,
+        payload: order,
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+};
+
+export const GetFilterByTeam = (info) => ({
+  ///
+  type: GET_FILTER_BY_TEAMS,
+  payload: info,
+});
+
+export function GetFilterCreatedDriver(infOrigin) {
+  ///
+  return {
+    type: GET_FILTER_CREATED_DRIVERS,
+    payload: infOrigin,
+  };
+}
+
+export const resetFilters = () => {
+  return {
+    type: RESET_FILTER,
+  };
+};
--- server/index.js
+++ server/index.js
@@ -1,10 +1,14 @@
 const axios = require("axios");
 const server = require("./src/server");
-const { conn } = require('./src/db.js');
-const PORT = 3001;
+const { conn } = require("./src/db.js");
+require("dotenv").config();
+const { PORT } = process.env || 3001;
 
-conn.sync({ force: false }).then(() => {
-server.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
-})
-}).catch(error => console.error(error))
+conn
+  .sync({ force: false })
+  .then(() => {
+    server.listen(PORT, () => {
+      console.log(`Server listening on port ${PORT}`);
+    });
+  })
+  .catch((error) => console.error(error));
